58269c3d3b7186a1adaf9bc52fe78b99640c75fa
hawaker <hawaker@gmail.com>
2017-05-19 15:25:02 +0800

    add README file

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view("index");
+});
 
 Route::any('{name}.html', function ($name) {
     return view($name);
